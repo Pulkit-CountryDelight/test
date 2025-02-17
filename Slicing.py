19b67e82aa6958e5097fcd106f4cfea2bddaa942
Pulkit Shamra <89565788+Pulkit-CountryDelight@users.noreply.github.com>
2025-02-17 16:06:10 +0530

    python
    learn python

---
+++ Slicing.py
@@ -0,0 +1,32 @@
+#Slicing  Program   +ve
+#lecture_two
+
+str1 = "This is a string"
+str2 = 'Apana College'
+str3 = """This is String"""
+
+print(str1)
+print(str2)
+print(str3)
+
+#concatenation
+str1 = "apna"
+str2 = " college"
+print(str1+str2)
+
+#length of str
+   str1 = "This is lenght of str program"
+   print(len(str1))
+
+
+#Indexing Program and slicing 
+   str1 = "this is indexing program"
+   print(str1[0:4])
+
+   print(str1[1:len(str1)])
+
+
+
+
+
+
